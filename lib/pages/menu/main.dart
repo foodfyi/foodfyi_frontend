21964640d1c6767ff72a4e8fcd0080922debafea
wcf <may.sincerity@outlook.com>
2022-11-22 15:58:16 -0500

    add app bar (#3)

--- lib/pages/menu/main.dart
+++ lib/pages/menu/main.dart
@@ -12,11 +12,48 @@ class MenuMain extends StatefulWidget {
 class _MenuMainState extends State<MenuMain> {
   int _selectedIndex = 0;
 
+  static final List<Widget> _appBarOptions = <Widget>[
+    // Menu
+    const Text('Edit Menu'),
+    // Comments
+    Column(
+      children: [
+        const Text(
+          'Comments',
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Icon(
+              Icons.star_rounded,
+              color: pinkHeavyColor,
+              size: 20.0,
+            ),
+            Text(
+              ' 4.5',
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: defaultPadding * 0.5),
+            ),
+            Text(
+              'xxx reviews',
+              style: TextStyle(fontSize: 14, color: greyHeavyColor),
+            ),
+          ],
+        ),
+      ],
+    ),
+    // Chat
+    const Text('Chat List'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('food.fyi'),
+        title: _appBarOptions[_selectedIndex],
+        centerTitle: true,
       ),
       drawer: const CommonDrawer(),
       bottomNavigationBar: Container(
@@ -31,7 +68,7 @@ class _MenuMainState extends State<MenuMain> {
             BottomNavigationBarItem(
                 icon: Icon(Icons.restaurant_menu_outlined), label: "Menu"),
             BottomNavigationBarItem(
-                icon: Icon(Icons.star_outline), label: "Comment"),
+                icon: Icon(Icons.star_outline_rounded), label: "Comment"),
             BottomNavigationBarItem(
                 icon: Icon(Icons.mail_outline), label: "Chat"),
           ],

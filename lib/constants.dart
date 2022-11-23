8a7e8ce0a1032eccfd762c9ea604d994cd7be7a1
JoannWu <37609013+JoanWu5@users.noreply.github.com>
2022-11-22 20:13:13 -0500

    add mock dish (#5)

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:foodfyi/models/allergy.dart';
 import 'package:foodfyi/models/dish.dart';
+import 'package:foodfyi/models/flavor.dart';
 
 const pinkColor = Color(0xFFF2C6C2);
 const pinkLightColor = Color(0xFFF2E8DF);
@@ -33,7 +35,137 @@ const int menuLoadSize = 15;
 
 // Mock Data
 final mockDishes = <Dish>[
-  Dish(id: 0, name: 'Burger & Soda', price: 8, rating: 5.0),
-  Dish(id: 1, name: 'Hotdog', price: 6, rating: 5.0),
-  Dish(id: 2, name: 'Sushi', price: 6, rating: 5.0),
+  Dish(
+    id: 0,
+    name: 'Oat Meal',
+    price: 8,
+    rating: 5.0,
+    allergyNoteIds: [5, 6, 7, 8],
+    flavorIds: [0],
+    imgUrl: [
+      'https://dining.columbia.edu/sites/default/files/2019-08/oatmeal.jpg',
+      'https://brokebankvegan.com/wp-content/uploads/2021/02/Mexican-Oatmeal-8.jpg',
+    ],
+  ),
+  Dish(
+    id: 1,
+    name: 'Bagels',
+    price: 6,
+    rating: 4.5,
+    imgUrl: [
+      'https://dining.columbia.edu/sites/default/files/2019-06/bagels.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/1/1d/Bagel-Plain-Alt.jpg'
+    ],
+    allergyNoteIds: [9],
+  ),
+  Dish(
+    id: 2,
+    name: 'Smoothies',
+    price: 6,
+    rating: 5.0,
+    allergyNoteIds: [5],
+    flavorIds: [0],
+    imgUrl: [
+      'https://dining.columbia.edu/sites/default/files/2020-03/healthy-green-smoothie-in-glass-picture-id1130419746.jpg',
+    ],
+  ),
+  Dish(
+    id: 3,
+    name: 'Scrambled Eggs',
+    price: 4,
+    rating: 4.8,
+    allergyNoteIds: [6, 7, 9],
+    imgUrl: [
+      'https://dining.columbia.edu/sites/default/files/2019-07/scrambled%20eggs%20cropped%202.jpg',
+    ],
+  ),
+  Dish(
+    id: 4,
+    name: 'PanCakes',
+    price: 7,
+    rating: 4.9,
+    allergyNoteIds: [7, 9],
+    flavorIds: [0],
+    imgUrl: [
+      'https://dining.columbia.edu/sites/default/files/2019-06/pancakes.jpeg',
+    ],
+  ),
+  Dish(
+    id: 5,
+    name: 'Roasted Cauliflower',
+    price: 7,
+    rating: 4.9,
+    allergyNoteIds: [5, 6, 7],
+    imgUrl: [
+      'https://dining.columbia.edu/sites/default/files/2019-07/roasted_cauliflower.jpg',
+    ],
+  ),
+  Dish(
+    id: 6,
+    name: 'Tater Tots',
+    price: 9,
+    rating: 4.7,
+    allergyNoteIds: [5, 7],
+    imgUrl: [
+      'https://dining.columbia.edu/sites/default/files/2019-07/iStock-576894832.jpg',
+    ],
+  ),
+  Dish(
+    id: 7,
+    name: 'Chicken Sausage',
+    price: 5,
+    rating: 4.7,
+    allergyNoteIds: [2, 6],
+    flavorIds: [6],
+    imgUrl: [
+      'https://dining.columbia.edu/sites/default/files/2019-07/breakfastsausage.ferris.everyday.jpg'
+    ],
+  ),
+  Dish(
+    id: 8,
+    name: 'French Toast',
+    price: 8,
+    rating: 5.0,
+    allergyNoteIds: [5],
+    flavorIds: [6],
+    imgUrl: [
+      'https://dining.columbia.edu/sites/default/files/2019-07/french%20toast_0.jpg',
+    ],
+  ),
+  Dish(
+    id: 9,
+    name: 'Waffles',
+    price: 3,
+    rating: 4.9,
+    allergyNoteIds: [5],
+    flavorIds: [0],
+    imgUrl: [
+      'https://dining.columbia.edu/sites/default/files/2019-07/waffles.ferris.everyday.jpg',
+      'https://goboldwithbutter.com/BoldWithButter/media/recipe_images/Imported/buttermilk-grits-waffles.jpg'
+    ],
+  ),
+];
+
+final mockAllergies = <Allergy>[
+  Allergy(id: 0, name: 'Beef'),
+  Allergy(id: 1, name: 'Pork'),
+  Allergy(id: 2, name: 'Chicken'),
+  Allergy(id: 3, name: 'Seafood'),
+  Allergy(id: 4, name: 'Lamb'),
+  Allergy(id: 5, name: 'Vegan'),
+  Allergy(id: 6, name: 'Gluten Free'),
+  Allergy(id: 7, name: 'Halal'),
+  Allergy(id: 8, name: 'Lactose'),
+  Allergy(id: 9, name: 'Egg'),
+];
+
+final mockFlavors = <Flavor>[
+  Flavor(id: 0, name: 'Sweet'),
+  Flavor(id: 1, name: 'Very Sweet'),
+  Flavor(id: 2, name: 'Sour'),
+  Flavor(id: 3, name: 'Very Sour'),
+  Flavor(id: 4, name: 'Spicy'),
+  Flavor(id: 5, name: 'Very Spicy'),
+  Flavor(id: 6, name: 'Salty'),
+  Flavor(id: 7, name: 'Very Salty'),
 ];
--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,7 @@ class MyApp extends StatelessWidget {
             borderRadius: border,
             borderSide: BorderSide.none,
           ),
+          floatingLabelStyle: TextStyle(color: pinkHeavyColor),
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
